d5eb273c0257ec582416b411746cbfe5d071d767
David de la Cruz <dcruz@mariabarcelona.com>
2021-07-28 10:29:48 +0200

    Add conditions to retry

--- src/receiver/receive.ts
+++ src/receiver/receive.ts
@@ -33,7 +33,9 @@ export class AmqpReceiver {
         /*If some error occurs retry de connection after 2 seconds with the same connection */
 
         return setTimeout(() => {
-          AmqpReceiver.connection(params);
+          if (!AmqpReceiver.CurrentConnection) {
+            AmqpReceiver.connection(params);
+          }
         }, 2000);
       }
     } catch (error) {
